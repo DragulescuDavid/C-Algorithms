57442c087fe453eee099b1a3fdc6051586713736
DragulescuDavid <48024809+DragulescuDavid@users.noreply.github.com>
2021-03-26 16:43:05 +0200

    Add files via upload

---
+++ StringContainsString/StringContainsString/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace StringContainsString
+{
+    class Program
+    {
+        static string isSubset(string[]a,string []b)
+        {
+            int i = 0;
+            int j = 0;
+            if (a.Length > b.Length) //if b has less elements than a, b cannot contain a
+                return "No";
+            for (i = 0; i < a.Length; i++) // serching elements from a in b
+            {
+                for (j = 0; j < b.Length; j++)
+                    if (a[i] == b[j])
+                        break;
+
+
+                if (j == b.Length) // because we used break before, if j reaches the length of b, at least one element from a is not in b
+                {
+                    
+                    return "No";
+                }
+            }
+
+           
+            return "Yes";
+        }
+        static void Main(string[] args)
+        {
+            string[] B = { "Purple", "Yellow" };
+            string[] A = { "Green", "Yellow", "Red", "Blue", "Purple"};
+            Console.WriteLine(isSubset(A, B));
+           
+        }
+    }
+}
