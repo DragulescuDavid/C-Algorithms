97f641f62a269fef0d78bc9a374e93b39854d8da
DragulescuDavid <48024809+DragulescuDavid@users.noreply.github.com>
2021-03-26 22:42:35 +0200

    Add files via upload

---
+++ BinarySearch/BinarySearch/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace BinarySearch
+{
+    class Program
+    {
+        public static bool BS(int[] array, int left, int right, int x)
+        //for searching a number x in array we split the array into half by comparing x to array[middle]
+        //to specify in which half of the array we should be searching(the array is sorted),
+        //and we do that until we find x or we can't split array anymore
+        {
+            if (left < right)
+            {
+                int middle = (left + right) / 2;
+                if (array[middle] == x)
+                {
+                    Console.WriteLine("Poisition: " + (middle + 1));
+                    return true;
+                }
+
+                else
+                {
+                    if (array[middle] > x)
+                        return BS(array, left, middle - 1, x);
+                    else
+                        return BS(array, middle + 1, right, x);
+                }
+
+            }
+            else
+                return false;
+        }
+        static void Main(string[] args)
+        {
+            int size = int.Parse(Console.ReadLine());
+            int[] array = new int[size];
+            int x;
+            bool final;
+            for (int i = 0; i < array.Length; ++i)
+                array[i] = int.Parse(Console.ReadLine());
+            x = int.Parse(Console.ReadLine());
+            final = BS(array, 0, array.Length, x);
+            if (final == true)
+                Console.WriteLine("Found");
+            else
+                Console.WriteLine("Not found");
+            Console.ReadKey();
+        }
+    }
+}
